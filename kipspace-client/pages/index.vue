e5e13760f3662516053ecb25deb7838ab94c0c6c
Theo Iyonor <symplytheo@gmail.com>
2020-09-08 23:07:34 +0100

    Fix 'avoid queue' section on homepage

--- kipspace-client/pages/index.vue
+++ kipspace-client/pages/index.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="home">
-    <v-container style="margin-top: 150px" class="mb-10">
+    <v-container style="margin-top: 100px" class="mb-15">
       <v-row justify="center" class="text-center">
         <v-col cols="12" md="10">
-          <h1 class="white--text">
+          <div class="display-1 white--text font-weight-bold">
             Make a reservation today.
             <br />Kip. Your. Space.
-          </h1>
+          </div>
         </v-col>
         <v-col cols="11" sm="8" lg="6">
           <v-text-field
             solo
             rounded
-            dense
             flat
+            height="54"
             label="Search or type location"
             append-icon="mdi-magnify"
             @keypress.enter="$router.push('/search')"
@@ -30,41 +30,39 @@
       <v-row justify="center" class="mt-10">
         <v-col cols="12" lg="10">
           <v-card class="pa-5 mb-10">
-            <v-row justify="center" class="pa-5 px-md-10">
-              <h3 class="primary--text text-center">
+            <v-row justify="center" class="px-5 px-md-10 mb-3">
+              <div class="title font-weight-bold primary--text text-center">
                 Avoid queues
-                <span class="secondary--text">.</span>
-              </h3>
-              <p class="text-center pt-2">
+              </div>
+              <div class="subtitle-1 text-center pt-2">
                 With Kipspace, you do not need to worry about waiting in queues.
                 Make reservations and gain easy access into any facility of your
                 choice.
-              </p>
+              </div>
             </v-row>
 
             <v-row>
               <v-container class="px-md-15">
                 <v-row>
-                  <v-col cols="12">
+                  <v-col cols="12" class="py-0">
                     <v-card
                       flat
-                      height="350px"
+                      height="250"
                       color="#F1F1F1"
                       class="ills-card-bg pa-5"
                     >
                       <v-row no-gutters align="center" class="fill-height">
-                        <v-col cols="6" md="8" class="pl-md-15">
-                          <div class="display-1 font-weight-bold primary--text">
-                            A new way to
-                            <br />kipspace
-                            <span class="secondary--text">.</span>
+                        <v-col cols="7" md="8" class="pl-md-15">
+                          <div class="headline font-weight-bold primary--text">
+                            A new way to <br />
+                            kipspace<span class="secondary--text pl-0">.</span>
                           </div>
-                          <h5 class="py-3 font-weight-thin">
+                          <div class="py-3 subtitle-1 font-weight-bold pr-md-10">
                             A digital tool for making reservations and managing
                             facilities.
-                          </h5>
+                          </div>
                         </v-col>
-                        <v-col cols="6" md="4">
+                        <v-col cols="5" md="4">
                           <v-img
                             src="/img/mobile-app.svg"
                             max-height="200"
@@ -83,7 +81,9 @@
     </v-container>
 
     <v-container class="mt-lg-10">
-      <h3>Top Searches</h3>
+      <div class="title font-weight-bold">
+        Top Searches
+      </div>
       <v-row justify="center">
         <v-col cols="12" sm="10" md="12">
           <v-row>
