659ffff26aeb68c53c7706d82a8c694833b18141
Isaac Oluwatemilorun <akiolisa@gmail.com>
2020-09-11 22:47:37 +0100

    fixed issues with user controllers

--- kipspace-server/src/controllers/user.controller/create-user.ts
+++ kipspace-server/src/controllers/user.controller/create-user.ts
@@ -1,11 +1,9 @@
 import User, { IUser } from '@models/User.model';
 import { CreateUser } from './types';
 
-export const createUser: CreateUser = async ({ firstname, lastname, email, hash }) => {
+export const createUser: CreateUser = async ({ email, hash }) => {
 	try {
 		const user: IUser = new User({
-			firstname: firstname.trim(),
-			lastname: lastname.trim(),
 			email: email.toLowerCase(),
 			hash,
 		});
