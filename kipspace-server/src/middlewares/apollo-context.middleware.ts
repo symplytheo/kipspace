a081631a97b55d3a7c946f3f2812c0424a1fb095
Isaac Oluwatemilorun <akiolisa@gmail.com>
2020-09-14 15:12:00 +0100

    Upated context middleware

--- kipspace-server/src/middlewares/apollo-context.middleware.ts
+++ kipspace-server/src/middlewares/apollo-context.middleware.ts
@@ -1,24 +1,33 @@
 import { Request } from 'express';
-import { AuthenticationError } from 'apollo-server-express';
 import { verify } from 'jsonwebtoken';
 import { JWT_SECRET } from '@config';
 // import { getOneUser } from '@controllers/user.controller';
 import { IUser } from '@models/User.model';
 
 export const ApolloContext = async ({ req }: { req: Request }) => {
+	let auth = true;
+	let decoded: IUser | null = null;
 	const token = req.headers.authorization;
 
-	if (!token) throw new AuthenticationError('No authorization token');
+	if (!token) auth = false;
+	else {
+		try {
+			decoded = verify(token, JWT_SECRET) as IUser;
 
-	try {
-		const decoded = verify(token, JWT_SECRET) as IUser;
-
-		// const await getOneUser({ email: decoded.email });
-		// TODO: Log authentication, session info and device info for user here
-		// TODO: Do a second round of token validation here to use single login
-
-		return decoded;
-	} catch (error) {
-		throw new AuthenticationError('Invalid token');
+			// const await getOneUser({ email: decoded.email });
+			// TODO: Log authentication, session info and device info for user here
+			// TODO: Do a second round of token validation here to use single login
+		} catch (error) {
+			auth = false;
+		}
 	}
+
+	return auth && decoded
+		? {
+				user: decoded,
+				auth,
+		  }
+		: {
+				auth,
+		  };
 };
