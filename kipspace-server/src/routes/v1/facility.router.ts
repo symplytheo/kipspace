2969f4354690b6391fd54071a98df1b2ae195201
Isaac Oluwatemilorun <akiolisa@gmail.com>
2020-09-20 23:59:21 +0100

    Created /facility/:id/cover route

---
+++ kipspace-server/src/routes/v1/facility.router.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import FacilityService from '@services/facility.service';
+// router.put('/update/:id/gallery', UserAuth, ImageUploader('facility/:id/gallery'), UploadFacilityGallery);
+
+const FacilityRouter = Router();
+const Facility = new FacilityService();
+
+// Expose request handlers to the service
+FacilityRouter.use((req, res, next) => {
+	Facility.Request = req;
+	Facility.Response = res;
+
+	next();
+});
+
+FacilityRouter.put(
+	'/:id/cover',
+	Facility.AuthenticateUser,
+	Facility.UploadFile('facility/:id/cover'),
+	Facility.UpdateCover
+);
+
+export default FacilityRouter;
