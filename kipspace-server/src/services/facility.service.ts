6864974c6ce4fb3b76ef465362e85c783c7d5d3b
Isaac Oluwatemilorun <akiolisa@gmail.com>
2020-09-21 00:07:34 +0100

    created update facility logo service

--- kipspace-server/src/services/facility.service.ts
+++ kipspace-server/src/services/facility.service.ts
@@ -3,7 +3,7 @@ import { OK, BAD_REQUEST } from 'http-status-codes';
 
 import { IFacility } from '@models/Facility.model';
 import { Exception } from '@shared/functions';
-import { UpdateFacilityCover } from '@daos/facility.dao';
+import { UpdateFacilityCover, UpdateFacilityLogo } from '@daos/facility.dao';
 
 import KipspaceService from './Kipspace.service';
 
@@ -21,6 +21,26 @@ class FacilityService extends KipspaceService<IFacility> {
 			this.SendResponse({
 				status: OK,
 				message: 'Cover updated successfully',
+				payload: (req.file as any).path,
+			});
+		} catch (error) {
+			this.SendError(error);
+		}
+	};
+
+	public UpdateLogo = async (req: Request): Promise<void> => {
+		try {
+			if (!req.file) throw new Exception(BAD_REQUEST, 'No or invalid image');
+
+			await UpdateFacilityLogo(
+				this.CurrentUser?._id,
+				req.params.id,
+				(req.file as any).path
+			);
+
+			this.SendResponse({
+				status: OK,
+				message: 'Logo updated successfully',
 				payload: (req.file as any).path,
 			});
 		} catch (error) {
