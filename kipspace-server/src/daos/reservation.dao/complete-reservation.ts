1d2bee28b2b1171c064ff4889b238e64175fc1a3
Isaac Oluwatemilorun <akiolisa@gmail.com>
2020-09-21 15:37:34 +0100

    created complete reservation dao

---
+++ kipspace-server/src/daos/reservation.dao/complete-reservation.ts
@@ -0,0 +1,59 @@
+import { GetOneFacility } from '@daos/facility.dao';
+import Reservation from '@models/Reservation.model';
+import { Exception } from '@shared/functions';
+import { BAD_REQUEST, NOT_FOUND } from 'http-status-codes';
+
+export const CompleteReservation = async (code: string) => {
+	try {
+		const reservation = await Reservation.findOne({ code });
+
+		if (!reservation) throw new Exception(NOT_FOUND, 'Reservation not found');
+
+		switch (reservation.current_status) {
+			case 'COMPLETED':
+				throw new Exception(BAD_REQUEST, 'Reservation has already been fulfilled.');
+
+			case 'CANCELED':
+				throw new Exception(BAD_REQUEST, 'Cannot complete a cancelled reservation.');
+
+			case 'MISSED':
+				throw new Exception(BAD_REQUEST, 'Cannot complete a missed reservation.');
+
+			case 'PENDING':
+				const reservationDate = new Date(reservation.date_reserved);
+				reservationDate.setHours(parseInt(reservation.time_reserved.split(':')[0]));
+				reservationDate.setMinutes(parseInt(reservation.time_reserved.split(':')[1]));
+
+				const layover = 900000; // 15 mins layover before closing reservation
+
+				if (reservationDate.getTime() > Date.now() + layover) {
+					await reservation.updateOne({
+						current_status: 'MISSED',
+						$addToSet: { status: 'MISSED' },
+					});
+
+					throw new Exception(BAD_REQUEST, 'Cannot complete a missed reservation.');
+				}
+
+				const facility = await GetOneFacility(reservation.facility);
+
+				if (!facility) throw new Exception(NOT_FOUND, 'Facility not found'); // this error should never be thrown
+
+				await facility.updateOne({
+					available_seats: facility.available_seats + reservation.seats,
+				});
+
+				await reservation.updateOne({
+					current_status: 'COMPLETED',
+					$addToSet: { status: 'COMPLETED' },
+				});
+
+				return reservation;
+
+			default:
+				throw new Exception(BAD_REQUEST, 'Reservation status cannot be determined.');
+		}
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
--- kipspace-server/src/daos/reservation.dao/index.ts
+++ kipspace-server/src/daos/reservation.dao/index.ts
@@ -1,1 +1,2 @@
 export * from './make-reservation';
+export * from './complete-reservation';
