ede605c9edcced005d54e7e348a3eb717367d1d2
Isaac Oluwatemilorun <akiolisa@gmail.com>
2020-09-21 15:35:18 +0100

    makeReservation mutation

--- kipspace-server/src/routes/graphql/mutations/reservation.ts
+++ kipspace-server/src/routes/graphql/mutations/reservation.ts
@@ -1,10 +1,41 @@
+import { UserInputError } from 'apollo-server-express';
 import { ReservationTC } from '@models/Reservation.model';
-import { AttachUserToArg, CheckAuth } from '@middlewares/resolver.middleware';
+import { AttachUserToArg, CheckAdmin, CheckAuth } from '@middlewares/resolver.middleware';
+import { MakeReservation } from '@daos/reservation.dao';
 
 export const ReservationMutation = {
-	createReservation: ReservationTC.getResolver('createOne', [
-		CheckAuth,
-		AttachUserToArg,
-	]),
-	updateReservation: ReservationTC.getResolver('updateById', [CheckAuth]),
+	makeReservation: ReservationTC.getResolver('createOne')
+		.wrap(
+			(resolver) => {
+				resolver.cloneArg('record', 'NewReservation');
+				resolver
+					.getArgITC('record')
+					.removeField(['user', 'code', 'current_status', 'statuses', '_id']);
+
+				return resolver;
+			},
+			{
+				displayName: 'MakeReservation',
+				description: 'Make a new reservation in a facility.',
+				args: { record: ReservationTC.getInputType() },
+				resolve: async ({ context, args }) => {
+					const reservation = await MakeReservation(
+						(context as any).user._id,
+						args.record.facility,
+						args.record.seats,
+						args.record.date_reserved,
+						args.record.time_reserved
+					).catch(({ message }) => {
+						throw new UserInputError(message);
+					});
+
+					// create reservation event here
+					return {
+						record: reservation,
+					};
+				},
+			}
+		)
+		.withMiddlewares([CheckAuth]),
+	updateReservation: ReservationTC.getResolver('updateById', [CheckAuth, CheckAdmin]),
 };
