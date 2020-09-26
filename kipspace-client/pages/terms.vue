06ab08afd05b20331a4411d63253cdd52db3064a
Theo Iyonor <symplytheo@gmail.com>
2020-09-26 21:58:22 +0100

    Format with prettier

--- kipspace-client/pages/terms.vue
+++ kipspace-client/pages/terms.vue
@@ -7,35 +7,50 @@
     </v-sheet>
     <v-container class="py-10">
       <p>
-        These Terms and Conditions constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and [business entity name] (“we,” “us” or “our”), concerning your access to and use of the [website name.com] website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”).
+        These Terms and Conditions constitute a legally binding agreement made
+        between you, whether personally or on behalf of an entity (“you”) and
+        [business entity name] (“we,” “us” or “our”), concerning your access to
+        and use of the [website name.com] website as well as any other media
+        form, media channel, mobile website or mobile application related,
+        linked, or otherwise connected thereto (collectively, the “Site”).
       </p>
       <p>
-        You agree that by accessing the Site, you have read, understood, and agree to be bound by all of these Terms and Conditions. If you do not agree with all of these Terms and Conditions, then you are expressly prohibited from using the Site and you must discontinue use immediately. 
+        You agree that by accessing the Site, you have read, understood, and
+        agree to be bound by all of these Terms and Conditions. If you do not
+        agree with all of these Terms and Conditions, then you are expressly
+        prohibited from using the Site and you must discontinue use immediately.
       </p>
       <p>
-        Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms and Conditions at any time and for any reason. 
+        Supplemental terms and conditions or documents that may be posted on the
+        Site from time to time are hereby expressly incorporated herein by
+        reference. We reserve the right, in our sole discretion, to make changes
+        or modifications to these Terms and Conditions at any time and for any
+        reason.
       </p>
       <p>
-        We will alert you about any changes by updating the “Last updated” date of these Terms and Conditions, and you waive any right to receive specific notice of each such change.
+        We will alert you about any changes by updating the “Last updated” date
+        of these Terms and Conditions, and you waive any right to receive
+        specific notice of each such change.
       </p>
       <p>
-        It is your responsibility to periodically review these Terms and Conditions to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms and Conditions by your continued use of the website thereafter.
+        It is your responsibility to periodically review these Terms and
+        Conditions to stay informed of updates. You will be subject to, and will
+        be deemed to have been made aware of and to have accepted, the changes
+        in any revised Terms and Conditions by your continued use of the website
+        thereafter.
       </p>
     </v-container>
-    
   </div>
 </template>
 
 <script>
 export default {
-head() {
+  head() {
     return {
-      title: 'Terms of Service'
-    };
-  }
+      title: 'Terms of Service',
+    }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
